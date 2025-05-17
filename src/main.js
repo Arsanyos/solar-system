49b2376c99490ac0c44643127e2239831c6e20cf
Arsanyos <arsanyos@convexit.net>
2025-05-17 14:14:02 +0300

    chore: add grid helper,update scene setup and add planet creation function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import * as THREE from "three";
-
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
-const scene = new THREE.Scene();
+const size = 50;
+const division = 100;
 
+const gridHelper = new THREE.GridHelper(size, division);
+
+const scene = new THREE.Scene();
+scene.add(gridHelper);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -20,10 +24,13 @@ const control = new OrbitControls(camera, renderer.domElement);
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
 
-camera.position.setZ(30);
+camera.position.setZ(20);
+camera.position.setX(0);
+camera.position.setY(80);
+control.update();
 renderer.render(scene, camera);
-//
 
 const textureLoader = new THREE.TextureLoader();
 const texture = textureLoader.load("../public/rock-wall-mortar_ao.png");
@@ -31,7 +38,7 @@ const textureDepth = textureLoader.load(
   "../public/rock-wall-mortar_normal-ogl.png"
 );
 
-const sunGeometry = new THREE.SphereGeometry(15, 32, 16);
+const sunGeometry = new THREE.SphereGeometry(5, 32, 16);
 const sunMaterial = new THREE.MeshPhongMaterial({
   map: texture,
   emissiveMap: textureDepth,
@@ -45,12 +52,38 @@ scene.add(sun);
 
 scene.add(pointLight, ambienLight);
 
+// write a function to create different plantets
+
+function createPlanet(radius, distance, color, speed) {
+  const geometry = new THREE.SphereGeometry(radius, 10, 10);
+  const material = new THREE.MeshBasicMaterial({ color });
+  const planet = new THREE.Mesh(geometry, material);
+
+  const orbit = new THREE.Group();
+  orbit.add(planet);
+  planet.position.x = distance;
+  scene.add(orbit);
+
+  return { planet, orbit, speed };
+}
+const earth = createPlanet(1.5, 15, 0x0000ff, 0.01);
+const mars = createPlanet(1.2, 10, 0xff0000, 0.008);
+
 function animate() {
   requestAnimationFrame(animate);
 
-  sun.rotation.x += 0.01;
-  sun.rotation.y += 0.01;
+  sun.rotation.y += 0.005;
+  //
+  earth.orbit.rotation.y += earth.speed;
+  mars.orbit.rotation.y += mars.speed;
+
   control.update();
   renderer.render(scene, camera);
 }
 animate();
+// 6. Handle window resize
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
